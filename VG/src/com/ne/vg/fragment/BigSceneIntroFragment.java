67cea94c9e18b0268a5f697c7526e8104dc470ea
hzc199307 <hzc199307@qq.com>
2014-08-21 22:17:53 +0800

    一些不兼容代码的提交

--- VG/src/com/ne/vg/fragment/BigSceneIntroFragment.java
+++ VG/src/com/ne/vg/fragment/BigSceneIntroFragment.java
@@ -3,7 +3,6 @@ package com.ne.vg.fragment;
 import com.ne.vg.R;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
